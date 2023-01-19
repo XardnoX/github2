c2edebb98771c8aa32a4fb68db118c6bec764060
XardnoX <ondra.kraja@gmail.com>
2023-01-19 12:11:16 +0100

    Initial commit

---
+++ src/cz/oauh/Main.java
@@ -0,0 +1,13 @@
+package cz.oauh;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+    List<Zakaznik> list = new ArrayList<>();
+    
+
+
+    }
+}
---
+++ src/cz/oauh/ulozeniEvidence.java
@@ -0,0 +1,5 @@
+package cz.oauh;
+
+public class ulozeniEvidence {
+
+}
